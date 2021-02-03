071dfb2191480cdc97236f299609e25883ac9d44
kevinbayter <kevinbayter@gmail.com>
2021-02-03 18:31:02 -0500

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,7 @@
 //Created by Kevin Bayter
 int main(void)
 {
-	char *holbertonStr = "\"Programming is like building a multilingual puzzle";
+	puts("\"Programming is like building a multilingual puzzle");
 
-	puts(holbertonStr);
 	return (0);
 }

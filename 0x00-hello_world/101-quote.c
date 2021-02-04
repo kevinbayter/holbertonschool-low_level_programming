728bc05cd833b75a26cc1c02cedc58e88247c8a3
kevinbayter <kevinbayter@gmail.com>
2021-02-03 21:08:42 -0500

    Thanks Vale <3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-//Created By Kevin Bayter
-
+/**
+ *main - variable sin retorno
+ *Return: 0
+ *Created By Kevin Bayter
+ */
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");

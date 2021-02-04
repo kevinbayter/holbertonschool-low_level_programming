0adf09cc3660a7e05e738e9b55101e9623a7c686
kevinbayter <kevinbayter@gmail.com>
2021-02-03 20:57:19 -0500

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *Kevin Bayter
+ * Return: Always 0
+ */
 
-//Created by Kevin Bayter
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");

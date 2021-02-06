863b31886be4bdf235b60748bc2d43928295e6c9
kevinbayter <kevinbayter@gmail.com>
2021-02-05 23:08:35 -0500

    10-print_comb2.c

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - metodo main
+ * main - Metodo main
  * Kevin Bayter
- * Return - 0
+ * Return: 0
  */
 
 int main(void)
